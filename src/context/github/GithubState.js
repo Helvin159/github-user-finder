39c25b3069d50040d3ff6e4e5d9468206ec8dc31
Helvin Rymer <Helvin159@gmail.com>
2021-06-28 18:17:14 -0400

    first react app

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -4,6 +4,18 @@ import GithubContext from './GithubContext';
 import GithubReducer from './GithubReducer';
 import { SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_USER, GET_REPOS } from '../Types';
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+	githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+	githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+}
+else {
+	githubClientId = process.env.GITHUB_CLIENT_ID;
+	githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = (props) => {
 	const initialState = {
 		users   : [],
@@ -21,8 +33,7 @@ const GithubState = (props) => {
 	const searchUsers = async (text) => {
 		setLoading();
 		const res = await axios.get(
-			`https://api.github.com/search/users?q=${text}&client_id=${process.env
-				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			`https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
 		);
 
 		dispatch({
@@ -36,8 +47,7 @@ const GithubState = (props) => {
 		setLoading(true);
 
 		const res = await axios.get(
-			`https://api.github.com/users/${username}?client_id=${process.env
-				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			`https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
 		);
 
 		dispatch({
@@ -51,8 +61,7 @@ const GithubState = (props) => {
 		setLoading(true);
 
 		const res = await axios.get(
-			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env
-				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
 		);
 
 		dispatch({
